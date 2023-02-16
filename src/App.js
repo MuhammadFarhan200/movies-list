3ef9af23be6f73f9e833c6ef6e0ac6568eb4fe67
MuhammadFarhan200 <farhan_200@smkassalaambandung.sch.id>
2023-02-16 23:12:08 +0700

    learn react js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import { getMovieList, searchMovie } from "./api";
+import { useEffect, useState } from 'react';
 
-function App() {
+const App = () => {
+  const [popularMovie, setPopularMovie] = useState([]);
+
+  useEffect(() => {
+    getMovieList().then((result) => {
+      setPopularMovie(result);
+    })
+  }, [])
+
+  const PopularMovieList = () => {
+    return popularMovie.map((movie, i) => {
+      return (
+        <div className="Movie-wrapper" key={i}>
+          <img className="Movie-image" src={`${process.env.REACT_APP_IMGURL}/${movie.poster_path}`} alt={movie.title} />
+          <div className="Movie-title">{movie.title}</div>
+          <div className='Row'>
+            <div className="Wrap-1">
+              <div className="Movie-date">{movie.release_date}</div>
+              <div className="Movie-language">{movie.original_language}</div>
+            </div>
+            <div className="Wrap-2">
+              <div className="Movie-rate">{movie.vote_average}</div>
+              <div className="Movie-count">({movie.vote_count})</div>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  }
+
+  const search = async (q) => {
+    const query = await searchMovie(q)
+    console.log({query: query});
+    setPopularMovie(query.results)
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Movie List
+        </h1>
+        <input
+          className="Movie-search"
+          placeholder="Ketikan sesuatu disini..."
+          onChange={({target}) => search(target.value)}
+        />
+        <div className="Movie-container">
+          <PopularMovieList />
+        </div>
       </header>
     </div>
   );
---
+++ src/api.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const apiKey = process.env.REACT_APP_APIKEY;
+const baseUrl = process.env.REACT_APP_APIURL;
+const genreUrl = process.env.REACT_APP_GENREURL;
+
+export const getMovieList = async () => {
+  const movie = await axios.get(`${baseUrl}/movie/popular?page=1&api_key=${apiKey}`); // API URL
+  return movie.data.results;
+}
+
+export const getGenreMovie = async () => {
+  const genre = await axios.get(`${genreUrl}/movie/list?page=1&api_key=${apiKey}`); // API URL
+  return genre.data.results;
+}
+
+export const searchMovie = async (q) => {
+  const search = await axios.get(`${baseUrl}/search/movie?api_key=${apiKey}&query=${q}`); // API URL
+  console.log(q);
+  return search.data;
+}
