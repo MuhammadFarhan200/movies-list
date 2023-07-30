a1086274d10405964afe678da0851f42725c67e0
MuhammadFarhan200 <farhannsrllh177@gmail.com>
2023-07-30 20:24:54 +0700

    no message

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -496,39 +496,37 @@ const Movies = () => {
   }
    
   return (
-    <>
-      <div className='container mx-auto p-5 lg:p-10' style={{ minHeight: 'calc(100vh - 128px)' }}>
-        <h3 className='text-sky-500 text-2xl sm:text-3xl md:text-4xl text-center font-semibold my-6'>List of Movies</h3>
-        <p className='text-slate-200 mb-6 max-w-3xl text-center mx-auto'>Come on, explore the complete list of film and find recommendations for your favorite films. Make your free time more enjoyable with us!</p>
-        <div className='flex mb-3 w-full sm:w-fit mx-auto space-x-3'>
-          <div className='relative flex w-full sm:w-fit' id='search-container'>
-            <input
-              className='bg-slate-800 text-slate-200 w-full sm:w-[30rem] h-12 ps-11 pe-5 rounded-lg outline-none focus:ring-2 focus:ring-cyan-500'
-              type='search'
-              name='search'
-              placeholder='Type movie title here...'
-              onChange={(e) => search(e.target.value)}
-              autoComplete="off"
-              value={isSearch ? searchQuery : ''}
-            />
-            <FontAwesomeIcon icon={faSearch} className='absolute left-4 top-1/2 transform -translate-y-1/2 text-slate-300' id='search-icon' />
-          </div>
-          <button className='button ms-auto' onClick={openModal}>
-            <FontAwesomeIcon icon={faSliders} className='md:me-2' />
-            <span className='hidden md:inline-block'>Filter</span>
-          </button>
+    <div className='container mx-auto p-5 lg:p-10' style={{ minHeight: 'calc(100vh - 128px)' }}>
+      <h3 className='text-sky-500 text-2xl sm:text-3xl md:text-4xl text-center font-semibold my-6'>List of Movies</h3>
+      <p className='text-slate-200 mb-6 max-w-3xl text-center mx-auto'>Come on, explore the complete list of film and find recommendations for your favorite films. Make your free time more enjoyable with us!</p>
+      <div className='flex mb-3 w-full sm:w-fit mx-auto space-x-3'>
+        <div className='relative flex w-full sm:w-fit' id='search-container'>
+          <input
+            className='bg-slate-800 text-slate-200 w-full sm:w-[30rem] h-12 ps-11 pe-5 rounded-lg outline-none focus:ring-2 focus:ring-cyan-500'
+            type='search'
+            name='search'
+            placeholder='Type movie title here...'
+            onChange={(e) => search(e.target.value)}
+            autoComplete="off"
+            value={isSearch ? searchQuery : ''}
+          />
+          <FontAwesomeIcon icon={faSearch} className='absolute left-4 top-1/2 transform -translate-y-1/2 text-slate-300' id='search-icon' />
         </div>
-        <p className='text-slate-300 text-center mb-6 sm:mb-10 '><span className='font-medium'>Note:</span> Filter feature cannot be combined with searching</p>
-        
-        <FilterModal />
-
-        <ShowCurrentFilter />
-
-        <PopularMovieList />
-        
-        <Pagination />
+        <button className='button ms-auto' onClick={openModal}>
+          <FontAwesomeIcon icon={faSliders} className='md:me-2' />
+          <span className='hidden md:inline-block'>Filter</span>
+        </button>
       </div>
-    </>
+      <p className='text-slate-300 text-center mb-6 sm:mb-10 '><span className='font-medium'>Note:</span> Filter feature cannot be combined with searching</p>
+      
+      <FilterModal />
+
+      <ShowCurrentFilter />
+
+      <PopularMovieList />
+      
+      <Pagination />
+    </div>
   );
 }
 
